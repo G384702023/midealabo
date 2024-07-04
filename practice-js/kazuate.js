59241298f50e554da6ae8e18034e621bc91d1311
G384702023 <238470@st.takushoku-u.ac.jp>
2024-07-04 16:50:49 +0900

    第5回

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -30,7 +30,7 @@ function hantei() {
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-  console.log(kaisu+'回目の予想'+ yoso);
+  console.log(kaisu+'回目の予想:'+ yoso);
   
   if(kaisu>3){
     console.log('答えは'+kotae+'でした。すでにゲームは終わっています');
