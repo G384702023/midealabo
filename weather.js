9c2c5cde33d5645d8fa0b62fa3754e4619c2fc06
G384702023 <238470@st.takushoku-u.ac.jp>
2024-07-25 00:37:42 +0900

    第6回

--- weather.js
+++ weather.js
@@ -1,68 +1,13 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
 
-////////// 課題3-2 ここからプログラムを書こう
 
-console.log(data.coord.lon);
-console.log(data.coord.lat);
-console.log(data.weather[0].description);
-console.log(data.main.temp_min);
-console.log(data.main.temp_max);
-console.log(data.main.humidity);
-console.log(data.wind.speed);
-console.log(data.wind.deg);
-console.log(data.name);
-
-let p1 = document.querySelector('div#result');
+function print(data) {
+  let p1 = document.querySelector('div#result');
 let li = document.createElement("li");
 li.textContent = "緯度:"+data.coord.lon+" 経度:"+data.coord.lat+" 天気"+data.weather[0].description
  +" 最低気温:"+data.main.temp_min+" 最高気温"+data.main.temp_max+" 湿度:"+data.main.humidity
  +" 風速:"+data.wind.speed+" 風向:"+data.wind.deg+" 都市名"+data.name;
 p1.insertAdjacentElement('beforeend', li);
+}
 
 let b = document.querySelector('button#btn');
 b.addEventListener('click', sendRequest);
@@ -77,11 +22,11 @@ function sendRequest() {
  let os = s.querySelectorAll('option');
  let o = os.item(idx);
  console.log('選択された ' +idx + ' 番目の option の情報:');
- console.log('  value' + o.getAttribute('value'));
+ console.log('  value=' + o.getAttribute('value'));
  console.log('  textContent='+o.textContent);
  let id = o.getAttribute('value');
 
- let url = 'https://www.nishita-lab.org/web-contents/jsons/openwether/'+ id + '.json';
+ let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+ id + '.json';
 
 
  axios.get(url)
@@ -91,11 +36,11 @@ function sendRequest() {
 }  
 
 function showResult(resp){
-  let date = resp.date;
-  if(typeof date ==='string'){
-    date =JSON.parse(date);
+  let data = resp.data;
+  if(typeof data ==='string'){
+    data =JSON.parse(data);
   }
-  print(date);
+  print(data);
 }
 
 
